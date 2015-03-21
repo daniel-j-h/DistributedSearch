9d9a9cba330a0f13ecc07837d98f1309bea4d32b
Daniel Hofmann <daniel@trvx.org>
2015-03-21 19:49:35 +0100

    Dependency on anl; already fixed upstream; drop static linking

--- Config.mk
+++ Config.mk
@@ -1,5 +1,5 @@
-#CXX = clang++-3.5
 CXX = g++-4.9
+CC = gcc-4.9
 CXXFLAGS += -pthread -fdiagnostics-color=auto -fmax-errors=1 -std=c++14 -O2 -g -Wall -Wextra -pedantic -Wuninitialized -Wstrict-overflow=3 -Wshadow -fno-omit-frame-pointer -fno-inline
-LDLIBS += -lstdc++ -pthread -static -lnanomsg -L/usr/local/lib/bond -lbond
+LDLIBS += -lstdc++ -pthread -lnanomsg -lanl -L/usr/local/lib/bond -lbond
 LDFLAGS += -Wl,-O1 -Wl,--hash-style=gnu -Wl,--sort-common -Wl,--demangle -Wl,--build-id
